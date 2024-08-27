c1d376b6ec5fbf91d4e1f2189a05772349c2f19a
Odi Ogali <odi.ogali1380@gmail.com>
2024-08-27 13:04:45 -0600

    Completely changed the ordering

--- main.c
+++ main.c
@@ -1,44 +1,61 @@
+/* main.c */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
-
-int main() {
-  // Create a TCP connection
-  int server_fd, new_socket;
-  struct sockaddr_in address;
-  int opt = 1;
-  int addrlen = sizeof(address);
-  char buffer[BUFFER_SIZE] = {0};
-
-  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-    perror("Error creating a new socket.");
-    return -1;
+int main(int argc, char *argv[]){ // two arguments, first - name of program, second - domain name
+  // Create a TCP connection:
+  
+  // Make sure there is only one argument
+  if (argc != 2) {
+    fprintf(stderr, "usage: httpC hostname\n");
+    return 1;
   }
 
-  // NOTE: Non-zero values, including -1 are true, while zero is false; we are checking for error: -1
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR|SO_REUSEPORT, &opt, sizeof(opt))) {
-    perror("Error setting the socket options.");
-    return -1;
+  int status;
+  struct addrinfo hints;
+  struct addrinfo *servInfo;
+  struct addrinfo *p;
+
+  memset(&hints, 0, sizeof hints); // zero out hints
+  hints.ai_family = AF_UNSPEC; // server machine may be IPv4 or IPv6
+  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+  hints.ai_flags = AI_PASSIVE; // fill in my IP for me
+  
+  if ((status = getaddrinfo(argv[1], "8080", &hints, &servInfo)) != 0){
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    return 2;
   }
 
-  // Accessing previously created address struct above
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
+  // We should now have a linked list of addrinfo structs - servInfo
+  
+  int serverfd;
 
-  address.sin_port = htons(PORT);
+  // Create a new socket so local machine (server) can communicate with clients
+  for (p  = servInfo; p != NULL; p = p->ai_next){
+    int s = socket(servInfo->ai_family, servInfo->ai_socktype, servInfo->ai_protocol);    
 
-  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-    perror("Failure binding socket to a IP address.");
-    return -1;
+    if (s != -1){
+      serverfd = s;
+      break;
+    }
   }
 
-  // The tutorial I was following, says to begin listening for a request here but I think, I did this wrong
-  // I should be the TCP client 
+  if (p == NULL) {
+    // No valid entries found
+    fprintf(stderr, "Failed to create socket.\n");
+    exit(3);
+  }
+  
+  // Set sock opt to enable reuse of a port after a server is closed
+  int yes = 1;
+  if (setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1){
+    fprintf(stderr, "Failed to set socket option.\n");
+    exit(4);
+  }
 
   return 0;
 }
